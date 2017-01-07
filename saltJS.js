afc0d1e0b7133264c9d7c26568b7cea5ed37ef78
Victoria <victoriabaeza18@gmail.com>
2017-01-07 18:04:22 -0500

    Nearly done besides aesthetic!

--- saltJS.js
+++ saltJS.js
@@ -4,7 +4,6 @@ function getReason() {
     $.get("https://torisaur.github.io/excuses.txt", function(data) {
         window.reasons = data.split('\n');
         var randomNumber = Math.floor(Math.random() * 12);
-        console.log(window.reasons[randomNumber]);
+        document.getElementById("saltReason").innerHTML = window.reasons[randomNumber];
     });
-    
 }
