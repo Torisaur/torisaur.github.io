4ca47c7c3a40d588b01e4ef7892c40f9246e8add
Victoria <victoriabaeza18@gmail.com>
2017-01-07 20:37:36 -0500

    Bug: only first 12 excuses showed.

--- js/saltJS.js
+++ js/saltJS.js
@@ -3,7 +3,7 @@ var reasons = ["test"];
 function getReason() {
     $.get("https://torisaur.github.io/resources/excuses.txt", function(data) {
         window.reasons = data.split('\n');
-        var randomNumber = Math.floor(Math.random() * 12);
+        var randomNumber = Math.floor(Math.random() *29);
         document.getElementById("saltReason").innerHTML = window.reasons[randomNumber];
     });
 }
